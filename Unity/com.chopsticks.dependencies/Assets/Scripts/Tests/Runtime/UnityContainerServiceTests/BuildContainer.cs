b5d1255317a96d0cef6892bc44da8da3aba0875c
Lucas Stertz <lucas@lucasstertz.com>
2025-02-12 14:56:43 -0600

    Add BuildContainer tests.

---
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Tests/Runtime/UnityContainerServiceTests/BuildContainer.cs
@@ -0,0 +1,51 @@
+﻿using Chopsticks.Dependencies.Services;
+using MonoContainerTests.Mocks;
+using NUnit.Framework;
+
+namespace UnityContainerServiceTests
+{
+    public class BuildContainer
+    {
+        [Test]
+        public void BuildContainer_WithDefinition_UsesFactoryWithDefintion()
+        {
+            // Arrange
+            var service = new UnityContainerService<MockDependencyContainer, 
+                MockDependencyContainerFactory, MockDependencyContainer.Definition>();
+            var definition = new MockDependencyContainer.Definition();
+
+            // Act
+            var container = service.BuildContainer(definition);
+
+            // Assert
+            var call = MockDependencyContainerFactory.Received.BuildContainerCall;
+            Assert.That(call.WasCalled, Is.True);
+            Assert.That(call.Parameter, Is.EqualTo(definition));
+            Assert.That(container, Is.Not.Null);
+
+            // Clean up
+            MockDependencyContainerFactory.ResetMockState();
+        }
+
+        [Test]
+        public void BuildContainer_WithoutDefinition_UsesFactoryWithDefault()
+        {
+            // Arrange
+            var service = new UnityContainerService<MockDependencyContainer, 
+                MockDependencyContainerFactory, MockDependencyContainer.Definition>();
+            MockDependencyContainer.Definition definition = default;
+
+            // Act
+            var container = service.BuildContainer();
+
+            // Assert
+            var call = MockDependencyContainerFactory.Received.BuildContainerCall;
+            Assert.That(call.WasCalled, Is.True);
+            Assert.That(call.Parameter, Is.EqualTo(definition));
+            Assert.That(container, Is.Not.Null);
+
+            // Clean up
+            MockDependencyContainerFactory.ResetMockState();
+        }
+    }
+}
