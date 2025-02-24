4000f4d6e59ee84f41c1584969211dbcdc6a52eb
Lucas Stertz <lucas@lucasstertz.com>
2025-02-24 14:45:20 -0600

    Increment release version to 0.3.0 to account for major new functionality and changes to the use of UnityContainerService.

--- Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/Contained/IUnityDependencyExtensions.cs
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/Contained/IUnityDependencyExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Chopsticks.Dependencies.Containers;
 using Chopsticks.Dependencies.Resolutions;
 using Chopsticks.Dependencies.Services;
-using Examples;
 using System;
 using UnityEngine;
 
@@ -87,6 +86,15 @@ namespace Chopsticks.Dependencies.Contained
         }
 
 
+        /// <summary>
+        /// Deregisters all of this dependency's registrations with its container.
+        /// </summary>
+        /// <typeparam name="TNativeContainer">The type of the internal, non-Unity 
+        /// dependency container.</typeparam>
+        /// <typeparam name="TUnityContainerService">The type of the Unity container service that 
+        /// provides Unity-specific services.</typeparam>
+        /// <param name="dependency">This dependency that is having its registrations 
+        /// deregistered.</param>
         public static void DeregisterAll<TNativeContainer, TUnityContainerService>(
             this IUnityDependency<TNativeContainer, TUnityContainerService> dependency)
             where TNativeContainer : IDependencyContainer, IDependencyResolutionProvider, IDisposable
@@ -97,6 +105,16 @@ namespace Chopsticks.Dependencies.Contained
             dependency.Registrations.Clear();
         }
 
+        /// <summary>
+        /// Registers this dependency as the specified contract for its container.
+        /// </summary>
+        /// <typeparam name="TNativeContainer">The type of the internal, non-Unity 
+        /// dependency container.</typeparam>
+        /// <typeparam name="TUnityContainerService">The type of the Unity container service that 
+        /// provides Unity-specific services.</typeparam>
+        /// <typeparam name="TContract">The contract type that the dependency 
+        /// is being registered as.</typeparam>
+        /// <param name="dependency">This dependency that is being registered.</param>
         public static DependencyRegistration RegisterAs<TNativeContainer, 
                 TUnityContainerService, TContract>(
             this IUnityDependency<TNativeContainer, TUnityContainerService> dependency)
