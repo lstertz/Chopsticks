976152069af028bfc154ee50b51ed0fccac7c387
Lucas Stertz <lucas@lucasstertz.com>
2025-03-07 14:45:40 -0600

    Add Shared as a static accessor to the main service instance for MonoContainerService.

--- Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/Services/MonoContainerService.cs
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/Services/MonoContainerService.cs
@@ -8,7 +8,15 @@ namespace Chopsticks.Dependencies.Services
     /// access to a global instance and strategies to work with other containers.
     /// </summary>
     public class MonoContainerService : UnityContainerService<
-        DependencyContainer, DefaultDependencyContainerFactory, DependencyContainerDefinition>
+        DependencyContainer, DefaultDependencyContainerFactory, DependencyContainerDefinition>,
+        IUnityContainerServiceProvider<DependencyContainer, MonoContainerService>
     {
+        /// <summary>
+        /// The service instance used across all standard <see cref="MonoDependency"/> 
+        /// and <see cref="MonoDependent"/> instances to integrate 
+        /// with <see cref="MonoContainer"/>s.
+        /// </summary>
+        public static MonoContainerService Shared =>
+            IUnityContainerServiceProvider<DependencyContainer, MonoContainerService>.InternalService;
     }
 }
