b132f37dbcfe49f3f5e2b4355ff27cb758c53716
Lucas Stertz <lucas@lucasstertz.com>
2025-03-14 15:10:09 -0500

    Simplify native system wrappers as MonoDependencies using the new methods.

---
+++ Unity/com.chopsticks.dependencies/Assets/Samples/Scripts/ExampleDependencies/ExampleSystem/Native/NativeSystemWrapper.cs
@@ -0,0 +1,16 @@
+﻿using Chopsticks.Dependencies;
+
+namespace Chopsticks.Samples.ExampleDependencies.ExampleSystem.Native
+{
+    /// <summary>
+    /// An example wrapper of a native system, to enable registration of 
+    /// a system that exists outside of Unity.
+    /// </summary>
+    public class NativeSystemWrapper : MonoDependency
+    {
+        protected override void PerformRegistration()
+        {
+            RegisterAs<IExampleSystem>(new NativeSystem());
+        }
+    }
+}
