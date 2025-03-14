24fc9bfe2e1611eb01bddb5dac24e625beb771e1
Lucas Stertz <lucas@lucasstertz.com>
2025-03-14 15:09:48 -0500

    Outline new convenience methods for registering native systems from a MonoDependency.

--- Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/BaseMonoDependency.cs
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Runtime/BaseMonoDependency.cs
@@ -15,8 +15,8 @@ namespace Chopsticks.Dependencies
         where TUnityContainerService : IUnityContainerService<TNativeContainer>, new()
     {
         ///<inheritdoc/>
-        List<DependencyRegistration> IUnityDependency<TNativeContainer, TUnityContainerService>.Registrations => 
-            _registrations;
+        List<DependencyRegistration> IUnityDependency<TNativeContainer, TUnityContainerService>
+            .Registrations => _registrations;
         private readonly List<DependencyRegistration> _registrations = new(1);
 
 
@@ -50,6 +50,48 @@ namespace Chopsticks.Dependencies
         /// This will be null if the attempt to register failed.</returns>
         protected DependencyRegistration RegisterAs<TContract>() =>
             this.RegisterAs<TNativeContainer, TUnityContainerService, TContract>();
+
+        /// <summary>
+        /// Registers the specified dependency as the specified contract.
+        /// </summary>
+        /// <remarks>
+        /// This should be used to enforce compile time type safety when registering 
+        /// a dependency, or when registering a dependency that is not a 
+        /// <see cref="IUnityDependency{TNativeContainer, TUnityContainerService}"/>, 
+        /// such as a non-Unity implementation.
+        /// </remarks>
+        /// <typeparam name="TContract">The contract to be registered as.</typeparam>
+        /// <param name="dependency">The dependency instance to be registered.</param>
+        /// <returns>The registration, to be used for manual deregistration, if needed.
+        /// This will be null if the attempt to register failed.</returns>
+        protected DependencyRegistration RegisterAs<TContract>(TContract dependency)
+        {
+            // TODO :: Implement extension method.
+            return null;
+        }
+
+        /// <summary>
+        /// Registers the specified implementation factory to fulfill the specified contract.
+        /// </summary>
+        /// <remarks>
+        /// This should be used when (likely non-Unity) dependencies that do not 
+        /// currently exist as explicit instances need to be registered.
+        /// </remarks>
+        /// <typeparam name="TContract">The contract to be registered as.</typeparam>
+        /// <param name="implementationFactory">The factory that will be 
+        /// registered to produce implementations that fulfill the contract as 
+        /// dependencies.</param>
+        /// <param name="lifetime">The lifetime that the registered 
+        /// dependency will have.</param>
+        /// <returns>The registration, to be used for manual deregistration, if needed.
+        /// This will be null if the attempt to register failed.</returns>
+        protected DependencyRegistration RegisterAs<TContract>(
+            Func<IDependencyContainer, TContract> implementationFactory,
+            DependencyLifetime lifetime = DependencyLifetime.Singleton)
+        {
+            // TODO :: Implement extension method.
+            return null;
+        }
 
 
         private void OnContainerSet()
