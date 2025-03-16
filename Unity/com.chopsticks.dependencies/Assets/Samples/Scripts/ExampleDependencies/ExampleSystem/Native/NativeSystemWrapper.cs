34ef116a1afabf648ddee77349995066961670c7
Lucas Stertz <lucas@lucasstertz.com>
2025-03-15 22:27:16 -0500

    Provide more detail to the NativeSystemWrapper in-code docs.

--- Unity/com.chopsticks.dependencies/Assets/Samples/Scripts/ExampleDependencies/ExampleSystem/Native/NativeSystemWrapper.cs
+++ Unity/com.chopsticks.dependencies/Assets/Samples/Scripts/ExampleDependencies/ExampleSystem/Native/NativeSystemWrapper.cs
@@ -10,9 +10,25 @@ namespace Chopsticks.Samples.ExampleDependencies.ExampleSystem.Native
     {
         private readonly NativeSystem _system = new();
 
+
+        /// <summary>
+        /// Register the native system for the wrapper's container.
+        /// </summary>
         protected override void PerformRegistration()
         {
             Register<IExampleSystem>(_system);
+        }
+
+        /// <summary>
+        /// Optionally implement to set up any dependencies that the native system may have.
+        /// </summary>
+        /// <remarks>
+        /// Alternatively, this could be done in the constructor of the native system by 
+        /// providing a factory that builds the native system with dependencies resolved 
+        /// from the container.
+        /// </remarks>
+        protected override void ResolveDependencies()
+        {
         }
     }
 }
