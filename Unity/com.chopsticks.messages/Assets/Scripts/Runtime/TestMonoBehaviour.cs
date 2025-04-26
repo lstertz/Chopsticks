4dab4f7f48bb0ce1555600bc2392180dc2f83795
Lucas Stertz <lucas@lucasstertz.com>
2025-04-26 17:35:47 -0500

    Add initial Messages Unity project.

---
+++ Unity/com.chopsticks.messages/Assets/Scripts/Runtime/TestMonoBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Chopsticks.Messages
+{
+    public class TestMonoBehaviour : MonoBehaviour
+    {
+        public Example Example { get; private set; }
+
+        public void Awake()
+        {
+            Example = new Example();
+
+            Debug.Log(Example.Test);
+        }
+    }
+}
---
+++ Unity/com.chopsticks.messages/Assets/Scripts/Tests/Editor/ExampleEditorTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Chopsticks.Messages.Tests
+{
+    public class ExampleEditorTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
