1e8673c302e701cf130a62c883f0862b8b457e33
Lucas Stertz <lucas@lucasstertz.com>
2025-06-25 23:03:35 -0500

    Detail expected async settings.

--- Native/Messages/AsyncSettings.cs
+++ Native/Messages/AsyncSettings.cs
@@ -2,6 +2,18 @@
 {
     public class AsyncSettings
     {
+        /// <summary>
+        /// Specifies whether handlers should be run in parallel or sequentially.
+        /// </summary>
         public bool RunParallel { get; set; }
+
+        /// <summary>
+        /// Specifies whether non-async handlers should be captured within 
+        /// async runners and executed asynchronously.
+        /// </summary>
+        /// <remarks>
+        /// If false, then non-async handlers will run synchronously while 
+        /// being represented by an async return value.</remarks>
+        public bool RunNonAsyncAsAsync { get; set; }
     }
 }
