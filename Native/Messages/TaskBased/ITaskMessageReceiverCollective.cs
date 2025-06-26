92978648c468002889f3e468a10afc10c2bdf6b2
Lucas Stertz <lucas@lucasstertz.com>
2025-06-25 21:57:04 -0500

    Correct namespace for ITaskMessageReceiverCollective.

--- Native/Messages/TaskBased/ITaskMessageReceiverCollective.cs
+++ Native/Messages/TaskBased/ITaskMessageReceiverCollective.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using Chopsticks.Messages.Abstractions;
+using System.Threading.Tasks;
 
-namespace Chopsticks.Messages.Abstractions
+namespace Chopsticks.Messages.TaskBased
 {
     public interface ITaskMessageReceiverCollective<TMessage> :
         IMessageReceiverCollective<TMessage, Task<MessageResult>>
