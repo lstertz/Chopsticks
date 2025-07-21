6f833f6871932e0d80763bdd4844af39c65d1844
Lucas Stertz <lucas@lucasstertz.com>
2025-07-20 21:56:10 -0500

    Refine HandlingStatus as flags.

--- Native/Messages/HandlingStatus.cs
+++ Native/Messages/HandlingStatus.cs
@@ -1,11 +1,52 @@
-﻿namespace Chopsticks.Messages
+﻿using System;
+
+namespace Chopsticks.Messages
 {
+    [Flags]
     public enum HandlingStatus
     {
-        Cancelled,
-        Failure,
-        NotHandled,
-        Processing,
-        Success
+        /// <summary>
+        /// The status of a handling process that is still in progress.
+        /// </summary>
+        Processing = 0,
+
+        /// <summary>
+        /// The status indicating that no handlers were found for the message.
+        /// </summary>
+        /// <remarks>
+        /// Under this status, no handling was performed, so it is considered 
+        /// neither a success, a failure, nor a cancellation; it never started and 
+        /// therefore never completed.
+        /// </remarks>
+        NotHandled = 1 << 0,
+
+
+        /// <summary>
+        /// The status indicating that the handling was successful.
+        /// </summary>
+        Success = 1 << 1,
+
+        /// <summary>
+        /// The status indicating that the handling failed with an exception.
+        /// </summary>
+        Failure = 1 << 2,
+
+        /// <summary>
+        /// The status indicating that the handling was cancelled before completion.
+        /// </summary>
+        Cancelled = 1 << 3,
+
+
+        /// <summary>
+        /// The status indicating that the handling was not successful, meaning that 
+        /// it started, but was either cancelled or failed.
+        /// </summary>
+        NonSuccess = Failure | Cancelled,
+
+        /// <summary>
+        /// The status indicating that the handling was completed, meaning that 
+        /// it started and ended, regardless of whether it was successful or not.
+        /// </summary>
+        Completed = Success | Failure | Cancelled,
     }
 }
