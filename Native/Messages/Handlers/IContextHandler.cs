6c0a63fd8adee867e95fcb8d6f24dc425fd0862c
Lucas Stertz <lucas@lucasstertz.com>
2025-08-10 22:25:14 -0500

    Initial context-based interfaces.

---
+++ Native/Messages/Handlers/IContextHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Chopsticks.Messages.Handlers
+{
+    public interface IContextHandler<TMessage, TContext>
+        where TContext : IMessageContext<TMessage>
+    {
+        HandlingPromise Handle(TContext context);
+
+        HandlingAwaitable HandleAsync(TContext context);
+    }
+
+    // TODO :: Make sync/async implementations.
+}
---
+++ Native/Messages/Handlers/IMessageContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+namespace Chopsticks.Messages.Handlers
+{
+    public interface IMessageContext<TMessage>
+    {
+        CancellationToken CancellationToken { get; }
+
+        TMessage Message { get; init; }
+    }
+}
