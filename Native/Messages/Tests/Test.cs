ad4d284e95280e56a6e5c0c761c9fda0a89f0e2f
Lucas Stertz <lucas@lucasstertz.com>
2025-09-08 08:15:45 -0500

    Add basic failure tests.

--- Native/Messages/Tests/Test.cs
+++ Native/Messages/Tests/Test.cs
@@ -14,14 +14,16 @@ namespace Tests
 
         public class MessageSender
         {
-            private IMessageHandler<Message> _handler;
+            private readonly IMessageHandler<Message> _handler;
 
             public MessageSender(IMessageHandler<Message> handler) =>
                 _handler = handler;
+
             public HandlingPromise Send()
             {
                 Console.WriteLine("Sending OnCommand");
-                var promise = _handler.Handle(new Message());
+                var promise = _handler.Handle(new Message())
+                    .ThrowIfFailed();
                 Console.WriteLine("Sent OnCommand");
 
                 return promise;
@@ -30,14 +32,33 @@ namespace Tests
             public async Task<HandlingResult> SendAsync()
             {
                 Console.WriteLine("Sending OnCommand");
-                var result = await _handler.HandleAsync(new Message());
+                var result = await _handler.HandleAsync(new Message())
+                    .ThrowIfFailed();
                 Console.WriteLine("Sent OnCommand");
 
                 return result;
             }
         }
 
-        public class SyncMessageHandler : ISyncMessageHandler<Message>
+        public class FailingSyncMessageHandler : ISyncMessageHandler<Message>
+        {
+            void ISyncMessageHandler<Message>.Handle(Message command)
+            {
+                throw new Exception("Simulated failure in sync handler.");
+            }
+        }
+
+        public class FailingAsyncMessageHandler : ITaskMessageHandler<Message>
+        {
+            async Task ITaskMessageHandler<Message>.HandleAsync(
+                Message message, CancellationToken token)
+            {
+                await Task.Delay(100, token); // Simulate async work.
+                throw new Exception("Simulated failure in async handler.");
+            }
+        }
+
+        public class SuccessfulSyncMessageHandler : ISyncMessageHandler<Message>
         {
             void ISyncMessageHandler<Message>.Handle(Message command)
             {
@@ -45,13 +66,13 @@ namespace Tests
             }
         }
 
-        public class AsyncMessageHandler : ITaskMessageHandler<Message>
+        public class SuccessfulAsyncMessageHandler : ITaskMessageHandler<Message>
         {
             async Task ITaskMessageHandler<Message>.HandleAsync(
                 Message message, CancellationToken token)
             {
                 Console.WriteLine($"Received OnCommand, Async, Value: {message.Value}.");
-                await Task.Delay(100); // Simulate async work.
+                await Task.Delay(100, token); // Simulate async work.
                 Console.WriteLine($"Done handling OnCommand, Async, Value: {message.Value}.");
             }
         }
@@ -62,15 +83,37 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
             // Act
+            var result = await sender.SendAsync();
+
+            // Assert
+            Assert.That(result.Status, Is.EqualTo(HandlingStatus.Success));
+        }
+
+        [Test]
+        public async Task Integration_MulticastAsyncCallAllAsyncTwice_Passes()
+        {
+            // Set up
+            var multicastHandler = new MulticastMessageHandler<Message>();
+            (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
+                new SuccessfulAsyncMessageHandler());
+            (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
+                new SuccessfulAsyncMessageHandler());
+            (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
+                new SuccessfulAsyncMessageHandler());
+
+            var sender = new MessageSender(multicastHandler);
+
+            // Act
+            _ = await sender.SendAsync();
             var result = await sender.SendAsync();
 
             // Assert
@@ -83,11 +126,11 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -104,12 +147,12 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler(), new RegistrationSettings()
+                new SuccessfulAsyncMessageHandler(), new RegistrationSettings()
                 {
                     Order = -1
                 });
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -126,12 +169,12 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler(), new RegistrationSettings()
+                new SuccessfulSyncMessageHandler(), new RegistrationSettings()
                 {
                     Order = -1
                 });
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -148,18 +191,22 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
             // Act
-            var promise = sender.Send();
+            _ = sender.Send();
 
             await Task.Delay(400);  // Make sure the async handlers finish.
+
+            var promise = sender.Send();
+
+            await Task.Delay(400);
 
             // Assert
             Assert.That(promise.Status, Is.EqualTo(HandlingStatus.Success));
@@ -171,11 +218,11 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -194,12 +241,12 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler(), new RegistrationSettings()
+                new SuccessfulAsyncMessageHandler(), new RegistrationSettings()
                 {
                     Order = -1
                 });
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler());
+                new SuccessfulSyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -218,12 +265,12 @@ namespace Tests
             // Set up
             var multicastHandler = new MulticastMessageHandler<Message>();
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new SyncMessageHandler(), new RegistrationSettings()
+                new SuccessfulSyncMessageHandler(), new RegistrationSettings()
                 {
                     Order = -1
                 });
             (multicastHandler as IMessageHandlerRegistrar<Message>).Register(
-                new AsyncMessageHandler());
+                new SuccessfulAsyncMessageHandler());
 
             var sender = new MessageSender(multicastHandler);
 
@@ -231,37 +278,132 @@ namespace Tests
             var promise = sender.Send();
 
             await Task.Delay(200);  // Make sure the async handler finishes.
+            promise = sender.Send();
+
+            await Task.Delay(200);  // Make sure the async handler finishes.
+            promise = sender.Send();
+
+            await Task.Delay(200);  // Make sure the async handler finishes.
 
             // Assert
             Assert.That(promise.Status, Is.EqualTo(HandlingStatus.Success));
         }
 
+
         [Test]
-        public async Task Integration_UniAsyncToSync_Passes()
+        public void UniAsyncToAsync_Failure_Throws()
         {
             // Set up
-            var handler = new SyncMessageHandler();
+            var handler = new FailingAsyncMessageHandler();
             var sender = new MessageSender(handler);
 
-            // Act
-            var result = await sender.SendAsync();
-
-            // Assert
-            Assert.That(result, Is.EqualTo(HandlingResult.Success));
+            // Act & Assert
+            Assert.ThrowsAsync<Exception>(sender.SendAsync);
         }
 
         [Test]
-        public async Task Integration_UniAsyncToAsync_Passes()
+        public void UniAsyncToSync_Failure_Throws()
         {
             // Set up
-            var handler = new AsyncMessageHandler();
+            var handler = new FailingSyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            // Act
+            Assert.ThrowsAsync<Exception>(sender.SendAsync);
+        }
+
+        [Test]
+        public void UniSyncToAsync_Failure_Throws()
+        {
+            // Set up
+            var handler = new FailingAsyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            bool hasCompleted = false;
+
+            // Act & Assert
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                var result = sender.Send();
+                result.OnCompletion(_ => hasCompleted = true);
+
+                while (!hasCompleted)  // Wait for async completion.
+                    await Task.Delay(10);
+            });
+        }
+
+        [Test]
+        public void UniSyncToSync_Failure_Throws()
+        {
+            // Set up
+            var handler = new FailingSyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => sender.Send());
+        }
+
+        [Test]
+        public async Task UniAsyncToAsync_Successful_Passes()
+        {
+            // Set up
+            var handler = new SuccessfulAsyncMessageHandler();
             var sender = new MessageSender(handler);
 
             // Act
             var result = await sender.SendAsync();
 
             // Assert
-            Assert.That(result, Is.EqualTo(HandlingResult.Success));
+            Assert.That(result.Status, Is.EqualTo(HandlingStatus.Success));
+        }
+
+
+        [Test]
+        public async Task UniAsyncToSync_Successful_Passes()
+        {
+            // Set up
+            var handler = new SuccessfulSyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            // Act
+            var result = await sender.SendAsync();
+
+            // Assert
+            Assert.That(result.Status, Is.EqualTo(HandlingStatus.Success));
+        }
+
+        [Test]
+        public async Task UniSyncToAsync_Successful_Passes()
+        {
+            // Set up
+            var handler = new SuccessfulAsyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            bool hasCompleted = false;
+
+            // Act
+            var result = sender.Send();
+            result.OnCompletion(_ => hasCompleted = true);
+
+            while (!hasCompleted)  // Wait for async completion.
+                await Task.Delay(10);
+
+            // Assert
+            Assert.That(result.Status, Is.EqualTo(HandlingStatus.Success));
+        }
+
+        [Test]
+        public void UniSyncToSync_Successful_Passes()
+        {
+            // Set up
+            var handler = new SuccessfulSyncMessageHandler();
+            var sender = new MessageSender(handler);
+
+            // Act
+            var result = sender.Send();
+
+            // Assert
+            Assert.That(result.Status, Is.EqualTo(HandlingStatus.Success));
         }
     }
 }
