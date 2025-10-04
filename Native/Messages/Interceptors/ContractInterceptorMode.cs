0792ca535cbbcd03195f117dcdc672ecb7a2f935
Lucas Stertz <lucas@lucasstertz.com>
2025-10-04 00:08:07 -0500

    Remove completed TODOs.

--- Native/Messages/Interceptors/ContractInterceptorMode.cs
+++ Native/Messages/Interceptors/ContractInterceptorMode.cs
@@ -1,13 +1,5 @@
 ﻿namespace Chopsticks.Messages.Interceptors
 {
-    // TODO :: See if shared and contract can be merged (if they should be merged).
-    // TODO :: Refactor interceptor adding to be done on a handler registration 
-    //             that is returned from registering a handler.
-    //             Registrations will need knowledge of their registrar to get 
-    //             the other interceptors to rebuild pipelines.
-    // TODO :: Refactor interceptor internals to work from IBaseInterceptor.
-
-
     public enum ContractInterceptorMode
     {
         Required,
